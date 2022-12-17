2c89df71e44622839032ddf5d54555dc9f00eef0
shunMT25 <120574907+shunMT25@users.noreply.github.com>
2022-12-17 13:34:05 +0900

    jsx more

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, {Component} from "react";
 
-// function App() {
-//   return (
-//     React.createElement("div", null, "Hello, world")
-//   );
-// }
-
 class App extends Component {
   render() {
-    return <div><h1>Hello, world!</h1></div>;
+    // const greeting = "Hi! Tom";
+    // const dom = <h1 className="foo">{greeting}</h1>
+    // const imput = <input type="text" onChange={() => {console.log("I am Tom")}}></input>
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input id="bar" type="text" onChange={() => {console.log("I am Tom")}}></input>
+      </React.Fragment>
+    );
   }
 }
-
-// class App extends Component {
-//   render() {
-//     return React.createElement(
-//       "div",
-//       null,
-//       "Hello, world!!"
-//     );
-//   }
-// }
 
 export default App;
