f680e7642b6b2a2cc5ea8d5b51dccdce44b5a42e
shunMT25 <120574907+shunMT25@users.noreply.github.com>
2022-12-17 14:55:56 +0900

    Components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React, {Component} from "react";
 
-class App extends Component {
-  render() {
-    // const greeting = "Hi! Tom";
-    // const dom = <h1 className="foo">{greeting}</h1>
-    // const imput = <input type="text" onChange={() => {console.log("I am Tom")}}></input>
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input id="bar" type="text" onChange={() => {console.log("I am Tom")}}></input>
-      </React.Fragment>
-    );
-  }
+// class App extends Component {
+//   render() {
+//     // const greeting = "Hi! Tom";
+//     // const dom = <h1 className="foo">{greeting}</h1>
+//     // const imput = <input type="text" onChange={() => {console.log("I am Tom")}}></input>
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input id="bar" type="text" onChange={() => {console.log("I am Tom")}}></input>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+
+const App = () => {
+  return (
+    <div>
+      <Cat/>
+      <Cat/>
+      <Cat/>
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
