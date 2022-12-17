d4b130603bef0bb0c3a8d7f82250082113367ce6
shunMT25 <120574907+shunMT25@users.noreply.github.com>
2022-12-17 12:34:28 +0900

    jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App"><h1>Hellow World</h1></div>
-  );
+// function App() {
+//   return (
+//     React.createElement("div", null, "Hello, world")
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return <div><h1>Hello, world!</h1></div>;
+  }
 }
+
+// class App extends Component {
+//   render() {
+//     return React.createElement(
+//       "div",
+//       null,
+//       "Hello, world!!"
+//     );
+//   }
+// }
 
 export default App;
