f01390f625a6bb9dbb6f6f3a16e36804f2922b92
shunMT25 <120574907+shunMT25@users.noreply.github.com>
2022-12-18 21:35:48 +0900

    complete redux-basics

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { legacy_createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+
 import './index.css';
-import App from './App';
+import reducer from './reducers'
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+const store = legacy_createStore(reducer)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
+  <Provider store={store}>
     <App />
+  </Provider>
   </React.StrictMode>
 );
 
